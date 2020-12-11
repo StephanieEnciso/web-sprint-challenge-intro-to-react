e8ff1c4fd7a747c7d42e9970c542b4525ad7bf6c
user.email <stephanie.enciso13@gmail.com>
2020-12-11 12:14:46 -0600

    Finished the components and I have rendered all the info to the page. Working on the CSS now.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect }from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +13,45 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character')
+    .then((res) => {
+      setCharacters(res.data.results);
+      console.log(res.data.results)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledH1 className="Header">Rick and Morty Characters</StyledH1>
+      {characters.map((character) =>{
+        return (
+          <StyledCharacterDiv>
+            <Character key = {character.id} info = {character} />
+          </StyledCharacterDiv>)
+      })}
     </div>
   );
 }
+
+const StyledH1 = styled.h1`
+  color: #a0e9f2;
+  text-shadow: 3px 3px darkslateblue;
+`;
+const StyledCharacterDiv = styled.div`
+  border: 4px solid black;
+  width: 70%;
+
+  img {
+    width: 20vw;
+    height: 32vh;
+  }
+`;
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+
+import React from 'react';
+
+
+export default function Character(props){
+    const {info} = props
+    return (
+        <div>
+            <div className='img-container'>
+                <img src = {info.image} alt = {info.name}/> 
+                <h4>{info.name}</h4>
+            </div>
+            <div>
+                    <p>Gender: {info.gender} </p>
+                    <p>Species: {info.species} </p>
+                    <p>Status: {info.status} </p>
+            </div>
+        </div>
+    )
+}
+
